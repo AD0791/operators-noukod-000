64d5261c42c0a5a90e7e5ee1516224f9c619cf6a
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-16 18:18:25 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
-  speed<40 || speed>60 ? true:false
+  if speed<40 || speed>60
+	  true
+	else
+	  false
+	end
 end
 
 
 
 def not_safe?(speed)
-	if speed<40 || speed>60
-	  true
-	else
-	  false
-	end
+  speed<40 || speed>60 ? true:false
 end
 	
 
